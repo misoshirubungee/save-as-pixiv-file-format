0b055a5fc7cafe8ff8a9b4f335816ca357106af0
d-ogita <ogita.39kv@outlook.jp>
2024-12-28 11:37:44 +0900

    first commit

---
+++ background.js
@@ -0,0 +1,45 @@
+chrome.runtime.onInstalled.addListener(() => {
+    chrome.contextMenus.create({
+        id: "saveRevasableImage",
+        title: "復元可能な名前を付けて保存",
+        contexts: ["image"]
+    });
+});
+
+chrome.contextMenus.onClicked.addListener(info => {
+    console.log(info);
+    if (info.menuItemId === "saveRevasableImage") {
+        const urlModule = new UrlModule(info.pageUrl);
+        const filename = urlModule.createFilename();
+        chrome.downloads.download({
+            url: info.srcUrl,
+            filename: filename
+        });
+    }
+});
+
+class UrlModule {
+    constructor(url) {
+        this.url = new URL(url);
+        this.hostname = this.url.hostname;
+        this.pathname = this.url.pathname;
+    }
+
+    createFilename() {
+        const pathList = this.pathname.split("/").filter(segment => segment);
+        switch(this.hostname) {
+            case "x.com": {
+                const POST_ID = 2;
+                const PAGE_ID = 4;
+                return `${pathList[POST_ID]}_x${pathList[PAGE_ID]}.jpg`;
+            }
+            case "fantia.jp": {
+                const POST_ID = 1;
+                const PAGE_ID = 3;
+                return `${pathList[POST_ID]}_f${pathList[PAGE_ID]}.jpg`;
+            }
+            default:
+                return "";
+        }
+    }
+}
